b75041bc62b87036fb15d66c79f1bbd1041fc0d8
ritika-abc <ritikasss321@gmail.com>
2025-04-26 17:01:51 +0530

    load

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -27,3 +27,18 @@ function makeTabActive() {
         }
     }
 }
+
+
+
+function contact() {
+    var na = document.querySelector(".name").value;
+    var em = document.querySelector(".email").value;
+ 
+    var mes = document.querySelector(".message").value;
+    var url1 = "https://wa.me/919911030329?text=" +
+        "Name: " + na + "%0A" +
+        "Email: " + em + "%0A" +       
+        "Message: " + mes;
+    window.open(url1, "_blank").focus();
+}
+
